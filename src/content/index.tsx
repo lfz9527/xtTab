66ff4a19094fbd60484639b5bcef85bb858666e5
lifangzheng <lifangzheng@lejurobot.com>
2026-07-31 13:41:14 +0800

    feat: 移除没必要代码

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,33 +1,9 @@
-import contentMessages from "@/messages/content"
+
 
 const Content = () => {
 
-    const handleSend = async () => {
-        contentMessages.send('tt', {
-            test: "3444"
-        })
-    }
-    const handleSendBg = async () => {
-        contentMessages.send('content_bg', {
-            test: "3444"
-        })
-    }
-
-    useEffect(() => {
-        contentMessages.on('t', ({ data }) => {
-            console.log(111, data)
-        })
-    }, [])
-
     return (
-        <div style={{
-            position: 'fixed',
-            zIndex: 999,
-            right: 0
-        }}>
-            <button onClick={handleSend}>content发送消息给siderPanel</button>
-            <button onClick={handleSendBg}>content发送消息给background</button>
-        </div>
+        <div />
     )
 }
 
